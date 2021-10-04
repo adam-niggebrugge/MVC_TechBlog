dbca479bd206cae09c47ca5fca6d0a68390018fd
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-04 18:14:34 -0400

    working on rendering data

--- controllers/view/home.js
+++ controllers/view/home.js
@@ -1,9 +1,25 @@
 const router = require('express').Router();
+const blog = require('../../utils/quick.json');
 
 router.get('/', async (req, res) => {
+    //fake session variable represent
+    const userId = 1;
+
     try{
-       res.render("home")
+        console.log(`is there data? ${JSON.stringify(blog)}`);
+
+        //mapping concept. same data plus additional isAuthor attribute of
+        // if coming from database must be seralirzed
+        const blogsMap = blog.map((blog) => {
+            return{
+                ...blog,
+                isAuthor: blog.user.userId === userId,
+            };
+       });
+       console.log(`what is going on?? ${JSON.stringify(blogsMap)}`);
+           res.render("home", {blog: blogsMap});
     } catch (err){
+        console.log(err)
         res.status(500).json(err);
     }
 });
