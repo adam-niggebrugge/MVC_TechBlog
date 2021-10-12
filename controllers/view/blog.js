6c6a912a24c564b38ccd61b2bb8937e677c53ad9
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-12 00:18:50 -0400

    objects are linking between pages. comments still need controllers to save

--- controllers/view/blog.js
+++ controllers/view/blog.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { Blog, Comment, User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
 
 router.get('/new', withAuth, async (req, res) => {
     try{
@@ -9,17 +11,30 @@ router.get('/new', withAuth, async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', withAuth, async (req, res) => {
     try{
-        res.render("blogPost")
+        const viewableBlog = await Blog.findOne({
+            where: {id: req.params.id},
+            attributes: ['id','title','body','date_created'],
+            include: [{ model: Comment, attributes: ['body', 'user_id'], 
+                    include: {model: User, attributes: ['name']}}],
+            include: [{model: User, attributes: ['name']}]
+        })
+        const sentBlog = viewableBlog.get({plain: true});
+        res.render("blogView", {blog: sentBlog, logged_in: req.session.logged_in})
     } catch (err){
         res.status(500).json(err);
     }
 });
 
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id', withAuth, async (req, res) => {
     try{
-        res.render("blogEdit")
+        const editableBlog = await Blog.findOne({
+            where: {id: req.params.id},
+            attributes: ['id','title','body','date_created']
+        })
+        const sentBlog = editableBlog.get({plain: true});
+        res.render("blogEdit", {blog: sentBlog, logged_in: req.session.logged_in})
     } catch (err){
         res.status(500).json(err);
     }
--- controllers/view/dashboard.js
+++ controllers/view/dashboard.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const { Blog, User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try{
-        res.render("dashboard", {logged_in : req.session.logged_in})
+        const blogData = await Blog.findAll({
+            include: [User],
+        });
+
+        const sendingblogs = blogData.map((blog) => blog.get({plain:true}));
+
+        res.render("dashboard", {blogs: sendingblogs, logged_in : true})
     } catch (err){
         res.status(500).json(err);
     }
